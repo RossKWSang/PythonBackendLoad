e3d8dc7ad6a7dab41864014bcaaf218cf3daa750
Wonsang Kim <rosskysoss@gmail.com>
2024-03-15 19:18:19 +0900

    backend and load test

--- load_test/locustfile.py
+++ load_test/locustfile.py
@@ -1,7 +1,26 @@
+import time
 from locust import HttpUser, task
+
 
 class HelloWorldUser(HttpUser):
     @task
-    def hello_world(self):
-        self.client.get("/hello")
-        self.client.get("/world")
+    def sign_up_test(self):
+        self.client.post(
+            "/sign-up",
+            json={
+                "email": "email@email.com",
+                "name": "송은우",
+                "password": "Qwerasdf1234!",
+                "profile": "안녕하세요 송은우입니다!"
+            }
+        )
+
+    @task
+    def tweet_test(self):
+        self.client.post(
+            "/tweet",
+            json={
+                "id": 1,
+                "tweet": "트윗 부하테스트"
+            }
+        )
