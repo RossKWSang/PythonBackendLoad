d0e3ce4491d30cd9b1ae971f04d97175ccd96e21
Wonsang Kim <rosskysoss@gmail.com>
2024-04-24 09:32:31 +0900

    [setting] remove regacy project

--- my_project/app/views.py
+++ my_project/app/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from app.models import Post
 
@@ -14,3 +14,10 @@ def index(request: HttpRequest) -> HttpResponse:
             "post_list": qs
         }
     )
+
+
+def post_detail(request: HttpRequest, post_id: int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=post_id)  # 예외처리
+    return render(request, "app/post_detail.html", {
+        "post": post
+    })
