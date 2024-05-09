b880aa53c384f941aaf68b54891cacf77f6f5fd7
Wonsang Kim <rosskysoss@gmail.com>
2024-05-09 23:11:10 +0900

    setting

--- myDjangoProject03-hottrack/mysite/settings.py
+++ myDjangoProject03-hottrack/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z3s@(&tyk6lqkgf#kcxc!zmxz8%)eg(zbibx^r=hkx(ou237wp'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
